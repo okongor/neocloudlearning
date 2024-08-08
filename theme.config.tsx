ca92a794634c78887601fea031c9d44cbc90798f
okongor <indaboski42@gmail.com>
2024-08-08 09:50:11 +0100

    fixed meta errors

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
 
@@ -126,7 +128,25 @@ const config: DocsThemeConfig = {
     defaultTheme: "light"
 
   },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://my-app.com' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
 
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'Nextra'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'The next site builder'}
+        />
+        <meta name="google-adsense-account" content="ca-pub-7094656317998541"></meta>
+      </>
+    )
+  }
 }
 
 export default config
